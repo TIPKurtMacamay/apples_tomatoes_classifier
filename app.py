154ffd6cdd4e438437119333de3e7070cbfd4b3a
TIPKurtMacamay <133735418+TIPKurtMacamay@users.noreply.github.com>
2023-10-17 20:36:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,33 +13,6 @@ import cv2
 from PIL import Image,ImageOps
 import numpy as np
 
-st.set_page_config(
-    page_title="Home",
-    page_icon="👋",
-)
-
-st.write("# Welcome to Apple and Tomato Agriculture Program! 👋")
-
-st.sidebar.success("Select a demo above.")
-
-st.markdown(
-    """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-"""
-)
-
 @st.cache_resource
 def load_model():
   model=tf.keras.models.load_model('weights-improvement-16-0.89.hdf5')
